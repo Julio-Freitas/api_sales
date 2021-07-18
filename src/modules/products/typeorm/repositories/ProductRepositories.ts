9d8d204f522cc407e79f2e60c554b1c0c7c94f34
Julio-Freitas <julio.clf31@gmail.com>
2021-07-18 20:39:36 -0300

    FIX:update branch

--- src/modules/products/typeorm/repositories/ProductRepositories.ts
+++ src/modules/products/typeorm/repositories/ProductRepositories.ts
@@ -3,8 +3,8 @@ import Product from '../entities/Product';
 type ProductType = Product | undefined;
 
 @EntityRepository(Product)
-export default class ProductRepository extends Repository<Product> {
-  static async findByName(name: string): Promise<ProductType> {
+class ProductRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<ProductType> {
     const product = this.findOne({
       where: {
         name,
@@ -13,3 +13,5 @@ export default class ProductRepository extends Repository<Product> {
     return product;
   }
 }
+
+export default ProductRepository;
